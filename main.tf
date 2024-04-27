76bbfa58c2d4df55e7f9729663870860af06dd29
Ricardo Vaz <50366220+ricardovazz@users.noreply.github.com>
2024-04-28 00:03:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
+terraform {
+    backend "gcs" { 
+      bucket  = "terraform-state-infrastructure-extended"
+      prefix  = "terraform/state"
+    }
+}
+
 provider "google" {
   project = "symbolic-datum-416912"
   region  = "europe-west2"
   zone    = "europe-west2-c"
-  credentials = file("/Users/rdvz/gcp-sandbox/symbolic-datum-416912-2d21e09abfc3.json")
 }
